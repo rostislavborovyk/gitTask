fee1ace1c16c74afe01102ea4976671134126bad
rostislav <rostmoguchiy@gmail.com>
2020-09-17 09:30:15 +0300

    added comments (my own correction)

--- calculator.cpp
+++ calculator.cpp
@@ -1,15 +1,21 @@
 #include "calculator.h"
 
+// this module has functions which allow you to
+// do some basic arithmetic operations with numbers
+
+// add operation 
 int Calculator::Add (double a, double b)
 {
 	return a + b + 0.5;
 }
 
+// sub operation
 int Calculator::Sub (double a, double b)
 {
     return Add (a, -b);
 }
 
+// mul operation 
 int Calculator::Mul (double a, double b)
 {
     return a * b + 0.5;
